67d498b743eff7c42379af9b97d6091a99ffa705
Loris Quetglas <36083989+LorisQUE@users.noreply.github.com>
2021-11-15 12:33:25 +0100

    Add RessourceType Dialog +Add and Edit RessourceType
    - We can now add and edit a type of resource

---
+++ src/java/montp/web/controllers/RessourceTypeDialog.java
@@ -0,0 +1,53 @@
+package montp.web.controllers;
+
+import montp.data.dao.RessourceTypeDAO;
+import montp.data.model.ressource.RessourceType;
+import montp.locale.Messages;
+import montp.services.RessourceTypeService;
+import montp.web.FacesTools;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@ViewScoped
+@Named("ressourceTypeDialog")
+public class RessourceTypeDialog implements Serializable {
+    @Inject protected Messages message;
+    @Inject protected RessourceTypeService service;
+
+    protected RessourceType instance;
+    protected RessourceTypeView ressourceTypeView;
+
+    public void create(RessourceTypeView ressourceTypeView) {
+        this.ressourceTypeView = ressourceTypeView;
+        instance = new RessourceType();
+    }
+
+    public void save() {
+        if (instance.getId() == null) {
+            service.insert(instance);
+            FacesTools.addMessage(FacesMessage.SEVERITY_INFO, message.get("app.added"));
+            ressourceTypeView.init();
+
+        } else {
+            service.update(instance);
+            FacesTools.addMessage(FacesMessage.SEVERITY_INFO, message.get("app.updated"));
+        }
+    }
+
+    public RessourceType getInstance() {
+        return instance;
+    }
+
+    public void setInstance(RessourceType instance) {
+        this.instance = instance;
+    }
+
+    public String getEditTitle(){
+        return String.format(message.get("app.ressourcetype.newTitle"), instance.toString());
+    }
+
+}
--- src/java/montp/web/controllers/RessourceTypeView.java
+++ src/java/montp/web/controllers/RessourceTypeView.java
@@ -3,8 +3,10 @@ package montp.web.controllers;
 import montp.data.model.ressource.RessourceType;
 import montp.services.RessourceTypeService;
 import montp.tools.Logger;
+import montp.web.FacesTools;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -16,14 +18,18 @@ import java.util.List;
 public class RessourceTypeView implements Serializable {
 
     @Inject private RessourceTypeService ressourceTypeService;
+    @Inject private RessourceTypeDialog ressourceTypeDialog;
+    private List<RessourceType> datas;
 
     @PostConstruct
     public void init() {
+        datas = ressourceTypeService.getRessourceTypes();
         Logger.log(Logger.LogLevel.INFO, RessourceTypeView.class.getSimpleName(), "initializing view controller");
     }
 
     public List<RessourceType> getRessourceTypes() {
-        return ressourceTypeService.getRessourceTypes();
+        //return ressourceTypeService.getRessourceTypes();
+        return datas;
     }
 
     public boolean canDelete(RessourceType ressourceType) {
@@ -34,4 +40,9 @@ public class RessourceTypeView implements Serializable {
         if(ressourceType != null)
             ressourceTypeService.delete(ressourceType);
     }
+
+    public void edit(RessourceType ressourceType) {
+        ressourceTypeDialog.setInstance(ressourceType);
+    }
+
 }
