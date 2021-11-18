e88419635f23781362c858941f7695756bfb6915
Loris Quetglas <36083989+LorisQUE@users.noreply.github.com>
2021-11-18 09:01:17 +0100

    Add Reservation View & Controller

---
+++ src/java/montp/web/controllers/ReservationView.java
@@ -0,0 +1,13 @@
+package montp.web.controllers;
+
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+
+public class ReservationView implements Serializable {
+
+    @PostConstruct
+    public void init(){
+        
+    }
+
+}
